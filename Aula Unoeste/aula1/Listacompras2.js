f6a31121785e0ee83c8c78fd6beb29be907742e9
Gustavo Costa <guccosta@gmail.com>
2024-01-04 12:59:21 -0300

    first commit

---
+++ Aula Unoeste/aula1/Listacompras2.js
@@ -0,0 +1,13 @@
+function adicionaLista(){
+    const botaoAdicionar = document.getElementById("botaoAdicionar");
+    const campoInput = document.getElementById("itens");
+    const ListaDeCompras = document.getElementById("#ListaDeCompras");
+
+    botaoAdicionar.addEventListener("click", function(event){
+      event.preventDefault()
+
+      const novoItem = campoInput
+
+    }
+
+}
